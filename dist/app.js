bef4c235f46c26c0bc5d53cbbcb379e541ae68f7
A1ien1385 <damian.zawadzki@tlen.pl>
2022-07-05 20:57:17 +0200

    typeScript todo app add styles

--- dist/app.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector("button");
-const calc = (originalPrice, hasDiscount) => {
-    return hasDiscount ? originalPrice * 0.8 : originalPrice;
-};
-button.addEventListener("click", () => {
-    const originalPrice = 50;
-    const hasDiscount = new URLSearchParams(window.location.search).get("discount") === "true";
-    console.log(calc(originalPrice, hasDiscount));
-});
