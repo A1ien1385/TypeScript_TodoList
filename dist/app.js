0c7ed3685efbc7de3031f6c7cfbbcac955b15705
A1ien1385 <damian.zawadzki@tlen.pl>
2022-07-05 15:56:27 +0200

    union type 2

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,7 @@
-//union type czyli zmienna może przyjąć albo string, albo number
+//union type czyli zmienna może przyjąć albo string, albo number, albo boolean
 const logAge = (age) => {
     console.log(`hej mam ${age} lat`);
 };
 logAge(24);
 logAge("dwadzieścia osiem");
+logAge(true);
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-//union type czyli zmienna może przyjąć albo string, albo number
-const logAge = (age: number | string) => {
+//union type czyli zmienna może przyjąć albo string, albo number, albo boolean
+const logAge = (age: number | string | boolean) => {
   console.log(`hej mam ${age} lat`);
 };
 
 logAge(24);
 logAge("dwadzieścia osiem");
+logAge(true);
