21b507aa69388bbbab4f98fd1dbd57a661ac4655
A1ien1385 <damian.zawadzki@tlen.pl>
2022-07-05 21:27:13 +0200

    typeScript todo app ad basic tasks

---
+++ dist/app.js
@@ -0,0 +1,24 @@
+// <li>
+//   <label for="task-1">Dać psu lekarstwa</label>
+//   <input type="checkbox" name="Dać psu lekarstwa" id="task-1" />
+// </li>;
+const tasksContainerElement = document.querySelector(".tasks");
+const tasks = [
+    "Dać psu lekarstwa",
+    "Wziąć Asamax",
+    "Zrobić lekcję Duolingo",
+    "Zjeść śniadanie",
+    "Wypić yerbe/kawę",
+    "Pouczyć się programowania",
+    "Porysować",
+    "Pojechać po Karolinę",
+    "Zagrać partyjkę w Elden Ring, albo poczytać Endymiona",
+];
+const render = () => {
+    tasks.forEach((task) => {
+        const taskElement = document.createElement("li");
+        taskElement.innerText = task;
+        tasksContainerElement.appendChild(taskElement);
+    });
+};
+render();
