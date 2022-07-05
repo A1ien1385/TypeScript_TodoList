bc7ae4200887e00366f4a46eff76d8816f51d8ea
A1ien1385 <damian.zawadzki@tlen.pl>
2022-07-05 17:03:43 +0200

    boolean w praktyce 2

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,9 @@
 const button = document.querySelector("button");
-
 const calc = (originalPrice, hasDiscount) => {
-  return hasDiscount ? originalPrice * 0.8 : originalPrice;
+    return hasDiscount ? originalPrice * 0.8 : originalPrice;
 };
-
 button.addEventListener("click", () => {
-  const originalPrice = 50;
-  const hasDiscount = new URLSearchParams(window.location.search).get(
-    "discount"
-  );
-  console.log(calc(originalPrice, hasDiscount));
+    const originalPrice = 50;
+    const hasDiscount = new URLSearchParams(window.location.search).get("discount") === "true";
+    console.log(calc(originalPrice, hasDiscount));
 });
